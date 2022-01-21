d12c2b25cccd1a591bf0b57103379ab65e023804
Futar <Futarimiti@163.com>
2022-01-21 15:54:56 +0000

    Remove `isCycleGraph` in class Graph as getter may not be correct

--- src/Graph.kt
+++ src/Graph.kt
@@ -10,19 +10,4 @@ class Graph(var edges : Array<Edge> = arrayOf() , var vertices : Array<Vertex> =
 		{
 			return this.edges.isEmpty()
 		}
-	
-	/**
-	 * determines if a graph is a cycle graph:
-	 * a graph with n vertices and n edges
-	 */
-	val isCycleGraph : Boolean
-		get()
-		{
-			for (e : Edge in edges)
-			{
-				if (e.isSelfLoop) return edges.size == 2
-			}
-			
-			return this.edges.size == this.vertices.size
-		}
 }
